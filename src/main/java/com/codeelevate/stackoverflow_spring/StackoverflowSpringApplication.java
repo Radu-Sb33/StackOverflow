ef55aae4c4933fecd4484fed9c3d117e820f9900
Sburlea Radu <sburlea.radu33@gmail.com>
2025-03-05 12:57:27 +0200

    First

---
+++ src/main/java/com/codeelevate/stackoverflow_spring/StackoverflowSpringApplication.java
@@ -0,0 +1,13 @@
+package com.codeelevate.stackoverflow_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackoverflowSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackoverflowSpringApplication.class, args);
+	}
+
+}
