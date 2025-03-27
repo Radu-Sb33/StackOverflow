04c5c4f1a0c6e514c20ad58d6a588a8867d93463
Sburlea Radu <sburlea.radu33@gmail.com>
2025-03-27 20:10:52 +0200

    login+signup

--- stackoverflow-angular/src/app/app.component.ts
+++ stackoverflow-angular/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LoginComponent} from "./auth-comp/login/login.component";
+import {SignupComponent} from "./auth-comp/signup/signup.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoginComponent,SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'stackoverflow-angular';
 }
